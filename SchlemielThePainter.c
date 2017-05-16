25d837b640d19c234a38a4ef582980f9be279623
Jasper Reichardt <jasperreichardt@gmail.com>
2017-05-16 14:14:25 +0200

    Create SchlemielThePainter.c

---
+++ SchlemielThePainter.c
@@ -0,0 +1,14 @@
+/* 
+This code uses the Shlemiel the painter’s algorithm. Who is Shlemiel? He’s the guy in this joke:
+
+Shlemiel gets a job as a street painter, painting the dotted lines down the middle of the road. On the first day he takes a can of paint out to the road and finishes 300 yards of the road. “That’s pretty good!” says his boss, “you’re a fast worker!” and pays him a kopeck.
+
+The next day Shlemiel only gets 150 yards done. “Well, that’s not nearly as good as yesterday, but you’re still a fast worker. 150 yards is respectable,” and pays him a kopeck.
+
+The next day Shlemiel paints 30 yards of the road. “Only 30!” shouts his boss. “That’s unacceptable! On the first day you did ten times that much work! What’s going on?”
+
+“I can’t help it,” says Shlemiel. “Every day I get farther and farther away from the paint can!”
+ - Copyright belonging to Joel Spolsky (thought he probably is happy about having this shared)
+*/
+
+//TODO
